88b46b8c53d822d3d1994a3d42efafd91c80fb7e
leonardo <leonardo@MASTERLEO>
2017-04-02 15:26:40 +0100

    Form de Login adicionado (visual)

---
+++ Projeto/formLogin.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Projeto
+{
+    partial class formLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formLogin));
+            this.gbLoginImg = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pbLogo = new System.Windows.Forms.PictureBox();
+            this.gbLoginDados = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtLoginUser = new System.Windows.Forms.TextBox();
+            this.txtLoginPass = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.gbLoginImg.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLogo)).BeginInit();
+            this.gbLoginDados.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbLoginImg
+            // 
+            this.gbLoginImg.Controls.Add(this.pbLogo);
+            this.gbLoginImg.Controls.Add(this.label1);
+            this.gbLoginImg.Dock = System.Windows.Forms.DockStyle.Left;
+            this.gbLoginImg.Location = new System.Drawing.Point(0, 0);
+            this.gbLoginImg.Name = "gbLoginImg";
+            this.gbLoginImg.Size = new System.Drawing.Size(219, 317);
+            this.gbLoginImg.TabIndex = 0;
+            this.gbLoginImg.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(40, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Arcmage";
+            // 
+            // pbLogo
+            // 
+            this.pbLogo.Image = ((System.Drawing.Image)(resources.GetObject("pbLogo.Image")));
+            this.pbLogo.Location = new System.Drawing.Point(12, 80);
+            this.pbLogo.Name = "pbLogo";
+            this.pbLogo.Size = new System.Drawing.Size(169, 214);
+            this.pbLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbLogo.TabIndex = 1;
+            this.pbLogo.TabStop = false;
+            // 
+            // gbLoginDados
+            // 
+            this.gbLoginDados.Controls.Add(this.btnSair);
+            this.gbLoginDados.Controls.Add(this.btnLogin);
+            this.gbLoginDados.Controls.Add(this.txtLoginPass);
+            this.gbLoginDados.Controls.Add(this.txtLoginUser);
+            this.gbLoginDados.Controls.Add(this.label3);
+            this.gbLoginDados.Controls.Add(this.label2);
+            this.gbLoginDados.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gbLoginDados.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbLoginDados.Location = new System.Drawing.Point(219, 0);
+            this.gbLoginDados.Name = "gbLoginDados";
+            this.gbLoginDados.Size = new System.Drawing.Size(288, 317);
+            this.gbLoginDados.TabIndex = 1;
+            this.gbLoginDados.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 22);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Utilizador";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 176);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 22);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Password";
+            // 
+            // txtLoginUser
+            // 
+            this.txtLoginUser.Location = new System.Drawing.Point(109, 104);
+            this.txtLoginUser.Name = "txtLoginUser";
+            this.txtLoginUser.Size = new System.Drawing.Size(156, 27);
+            this.txtLoginUser.TabIndex = 2;
+            // 
+            // txtLoginPass
+            // 
+            this.txtLoginPass.Location = new System.Drawing.Point(109, 173);
+            this.txtLoginPass.Name = "txtLoginPass";
+            this.txtLoginPass.PasswordChar = '*';
+            this.txtLoginPass.Size = new System.Drawing.Size(156, 27);
+            this.txtLoginPass.TabIndex = 3;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(193, 260);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(72, 34);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Entrar";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(109, 260);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(72, 34);
+            this.btnSair.TabIndex = 5;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // formLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(507, 317);
+            this.Controls.Add(this.gbLoginDados);
+            this.Controls.Add(this.gbLoginImg);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(525, 364);
+            this.MinimumSize = new System.Drawing.Size(525, 364);
+            this.Name = "formLogin";
+            this.Text = "Arcmage - Login";
+            this.gbLoginImg.ResumeLayout(false);
+            this.gbLoginImg.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLogo)).EndInit();
+            this.gbLoginDados.ResumeLayout(false);
+            this.gbLoginDados.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbLoginImg;
+        private System.Windows.Forms.PictureBox pbLogo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox gbLoginDados;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.TextBox txtLoginPass;
+        private System.Windows.Forms.TextBox txtLoginUser;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Projeto/formLogin.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Projeto
+{
+    public partial class formLogin : Form
+    {
+        public formLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            formMenuAdmin menu = new formMenuAdmin();
+
+            menu.Show();
+            Hide();
+        }
+    }
+}
