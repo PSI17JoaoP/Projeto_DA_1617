c1024229f997127ab7f870e79a484556ec740ea1
João Porêlo <2160453@my.ipleiria.pt>
2017-04-15 16:04:37 +0100

    Utilizadores - Login Funcional

--- Projeto/formLogin.Designer.cs
+++ Projeto/formLogin.Designer.cs
@@ -50,17 +50,20 @@
             this.gbLoginImg.Controls.Add(this.label1);
             this.gbLoginImg.Dock = System.Windows.Forms.DockStyle.Left;
             this.gbLoginImg.Location = new System.Drawing.Point(0, 0);
+            this.gbLoginImg.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.gbLoginImg.Name = "gbLoginImg";
-            this.gbLoginImg.Size = new System.Drawing.Size(219, 317);
+            this.gbLoginImg.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.gbLoginImg.Size = new System.Drawing.Size(164, 265);
             this.gbLoginImg.TabIndex = 0;
             this.gbLoginImg.TabStop = false;
             // 
             // pbLogo
             // 
             this.pbLogo.Image = ((System.Drawing.Image)(resources.GetObject("pbLogo.Image")));
-            this.pbLogo.Location = new System.Drawing.Point(12, 80);
+            this.pbLogo.Location = new System.Drawing.Point(9, 65);
+            this.pbLogo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pbLogo.Name = "pbLogo";
-            this.pbLogo.Size = new System.Drawing.Size(169, 214);
+            this.pbLogo.Size = new System.Drawing.Size(127, 174);
             this.pbLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbLogo.TabIndex = 1;
             this.pbLogo.TabStop = false;
@@ -69,9 +72,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(40, 18);
+            this.label1.Location = new System.Drawing.Point(30, 15);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(109, 29);
+            this.label1.Size = new System.Drawing.Size(87, 24);
             this.label1.TabIndex = 0;
             this.label1.Text = "Arcmage";
             // 
@@ -85,77 +89,86 @@
             this.gbLoginDados.Controls.Add(this.label2);
             this.gbLoginDados.Dock = System.Windows.Forms.DockStyle.Fill;
             this.gbLoginDados.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.gbLoginDados.Location = new System.Drawing.Point(219, 0);
+            this.gbLoginDados.Location = new System.Drawing.Point(164, 0);
+            this.gbLoginDados.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.gbLoginDados.Name = "gbLoginDados";
-            this.gbLoginDados.Size = new System.Drawing.Size(288, 317);
+            this.gbLoginDados.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.gbLoginDados.Size = new System.Drawing.Size(218, 265);
             this.gbLoginDados.TabIndex = 1;
             this.gbLoginDados.TabStop = false;
             // 
             // btnSair
             // 
-            this.btnSair.Location = new System.Drawing.Point(109, 260);
+            this.btnSair.Location = new System.Drawing.Point(82, 211);
+            this.btnSair.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnSair.Name = "btnSair";
-            this.btnSair.Size = new System.Drawing.Size(72, 34);
+            this.btnSair.Size = new System.Drawing.Size(54, 28);
             this.btnSair.TabIndex = 5;
             this.btnSair.Text = "Sair";
             this.btnSair.UseVisualStyleBackColor = true;
-            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            this.btnSair.Click += new System.EventHandler(this.BotaoSairAplicacao);
             // 
             // btnLogin
             // 
-            this.btnLogin.Location = new System.Drawing.Point(193, 260);
+            this.btnLogin.Location = new System.Drawing.Point(145, 211);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(72, 34);
+            this.btnLogin.Size = new System.Drawing.Size(54, 28);
             this.btnLogin.TabIndex = 4;
             this.btnLogin.Text = "Entrar";
             this.btnLogin.UseVisualStyleBackColor = true;
-            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            this.btnLogin.Click += new System.EventHandler(this.BotaoLoginUtilizador);
             // 
             // txtLoginPass
             // 
-            this.txtLoginPass.Location = new System.Drawing.Point(109, 173);
+            this.txtLoginPass.Location = new System.Drawing.Point(82, 141);
+            this.txtLoginPass.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtLoginPass.Name = "txtLoginPass";
             this.txtLoginPass.PasswordChar = '*';
-            this.txtLoginPass.Size = new System.Drawing.Size(156, 27);
+            this.txtLoginPass.Size = new System.Drawing.Size(118, 23);
             this.txtLoginPass.TabIndex = 3;
             // 
             // txtLoginUser
             // 
-            this.txtLoginUser.Location = new System.Drawing.Point(109, 104);
+            this.txtLoginUser.Location = new System.Drawing.Point(82, 84);
+            this.txtLoginUser.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtLoginUser.Name = "txtLoginUser";
-            this.txtLoginUser.Size = new System.Drawing.Size(156, 27);
+            this.txtLoginUser.Size = new System.Drawing.Size(118, 23);
             this.txtLoginUser.TabIndex = 2;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(22, 176);
+            this.label3.Location = new System.Drawing.Point(16, 143);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(71, 22);
+            this.label3.Size = new System.Drawing.Size(58, 17);
             this.label3.TabIndex = 1;
             this.label3.Text = "Password";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(22, 107);
+            this.label2.Location = new System.Drawing.Point(16, 87);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(68, 22);
+            this.label2.Size = new System.Drawing.Size(52, 17);
             this.label2.TabIndex = 0;
             this.label2.Text = "Utilizador";
             // 
             // formLogin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(507, 317);
+            this.ClientSize = new System.Drawing.Size(382, 265);
             this.Controls.Add(this.gbLoginDados);
             this.Controls.Add(this.gbLoginImg);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(525, 364);
-            this.MinimumSize = new System.Drawing.Size(525, 364);
+            this.MaximumSize = new System.Drawing.Size(398, 303);
+            this.MinimumSize = new System.Drawing.Size(398, 303);
             this.Name = "formLogin";
             this.Text = "Arcmage - Login";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.formLogin_FormClosed);
--- Projeto/formLogin.cs
+++ Projeto/formLogin.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,27 +13,122 @@ namespace Projeto
 {
     public partial class formLogin : Form
     {
+        public DiagramaArcmageContainer containerDados;
+
         public formLogin()
         {
             InitializeComponent();
-        }
 
-        private void btnSair_Click(object sender, EventArgs e)
-        {
-            Application.Exit();
-        }
-
-        private void btnLogin_Click(object sender, EventArgs e)
-        {
-            formMenuAdmin menu = new formMenuAdmin();
-
-            menu.Show();
-            Hide();
+            containerDados = new DiagramaArcmageContainer();
         }
 
         private void formLogin_FormClosed(object sender, FormClosedEventArgs e)
         {
             Application.Exit();
+        }
+
+        private void BotaoSairAplicacao(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void BotaoLoginUtilizador(object sender, EventArgs e)
+        {
+            if (txtLoginUser.Text.Length > 0 && txtLoginPass.Text.Length > 0)
+            {
+                string usernameForm = txtLoginUser.Text;
+
+                string passwordForm = HashPassword(txtLoginPass.Text);
+
+                foreach(User utilizador in containerDados.UserSet)
+                {
+                    if(usernameForm == utilizador.Username && passwordForm == utilizador.Password)
+                    {
+                        int idUtilizador = utilizador.Id;
+
+                        if (VerificarTipoAdministrator(idUtilizador))
+                        {
+                            formMenuAdmin menuAdmin = new formMenuAdmin();
+                            menuAdmin.Show();
+                            Hide();
+                        }
+
+                        else
+                        {
+                            formMenuArbitro menuArbitro = new formMenuArbitro();
+                            menuArbitro.Show();
+                            Hide();
+                        }
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Método de encriptação da password de um utilizador.
+        /// Encripta a password em hash de SHA1 e devolve a hash da password.
+        /// </summary>
+        /// <param name="password">Password do utilizador</param>
+        /// <returns></returns>
+        private string HashPassword(string password)
+        {
+            string passwordHash;
+
+            using (SHA1 sha1Algorithm = new SHA1CryptoServiceProvider())
+            {
+                byte[] dadosBytes = Encoding.UTF8.GetBytes(password);
+                byte[] hashBytes = sha1Algorithm.ComputeHash(dadosBytes);
+
+                passwordHash = BitConverter.ToString(hashBytes);
+            }
+
+            return passwordHash;
+        }
+
+        /// <summary>
+        /// Método para verificar se a linha da tabela na tab dos Utilizadores é do tipo Administrator.
+        /// Verifica se o id da linha selecionada é igual ao id de um administrador.
+        /// Se for igual, a variavel Boolean "isTipoAdministrator" é igual a true.
+        /// No final, retorna a variavel anterior.
+        /// </summary>
+        /// <param name="idUtilizador">Id do Utilizador</param>
+        /// <returns>Variavel Boolean "isTipoAdministrator"</returns>
+        private bool VerificarTipoAdministrator(int idUtilizador)
+        {
+            bool isTipoAdministrator = false;
+
+            foreach (Administrator admin in containerDados.UserSet.OfType<Administrator>())
+            {
+                if (admin.Id == idUtilizador)
+                {
+                    isTipoAdministrator = true;
+                }
+            }
+
+            return isTipoAdministrator;
+        }
+
+        /// <summary>
+        /// Método para verificar se a linha da tabela na tab dos Utilizadores é do tipo Referee.
+        /// Verifica se o id da linha selecionada é igual ao id de um arbitro.
+        /// Se for igual, a variavel Boolean "isTipoArbitro" é igual a true.
+        /// No final, retorna a variavel anterior.
+        /// </summary>
+        /// <param name="idUtilizador">Id do Utilizador</param>
+        /// <returns>Variavel Boolean "isTipoArbitro"</returns>
+        private bool VerificarTipoReferee(int idUtilizador)
+        {
+            bool isTipoArbitro = false;
+
+            foreach (Referee arbitro in containerDados.UserSet.OfType<Referee>())
+            {
+                if (arbitro.Id == idUtilizador)
+                {
+                    isTipoArbitro = true;
+                }
+            }
+
+            return isTipoArbitro;
         }
     }
 }
